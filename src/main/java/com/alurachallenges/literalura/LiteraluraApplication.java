db178ffca180e8d9ef2ffe3ad93ef3b38e11f1d1
Fredy Amaya <feat0429f@gmail.com>
2024-05-16 11:14:07 -0600

    Added application entry point.

--- src/main/java/com/alurachallenges/literalura/LiteraluraApplication.java
+++ src/main/java/com/alurachallenges/literalura/LiteraluraApplication.java
@@ -1,13 +1,24 @@
 package com.alurachallenges.literalura;
 
+import com.alurachallenges.literalura.presentation.ConsoleUi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private ConsoleUi consoleUi;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) {
+		consoleUi.displayMenu();
+	}
 }
